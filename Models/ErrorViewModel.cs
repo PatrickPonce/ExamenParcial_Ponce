208e0e2e8b37a1829d3c8d54c4ed190bd9d7ed4f
Patrick Ponce <patrick_ponce1@usmp.pe>
2025-10-04 12:24:45 -0500

    Proyecto Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExamenParcial_Ponce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
